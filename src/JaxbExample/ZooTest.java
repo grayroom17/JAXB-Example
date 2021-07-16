4c0aa4eadfe066981df99392919055a0eb080f8f
Sergey Deev <mygrayroom@gmail.com>
2021-07-16 10:49:17 +0300

    Add JUnit tests

---
+++ src/JaxbExample/ZooTest.java
@@ -0,0 +1,20 @@
+package JaxbExample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ZooTest {
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void createZoo() {
+    }
+
+    @Test
+    public void marshalToXmlString() {
+    }
+}
